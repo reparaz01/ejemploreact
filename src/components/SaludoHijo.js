f83822442fc208b32dca35433c5a250f4bc49be0
reparaz01 <jreparazm@gmail.com>
2023-10-16 14:09:23 +0200

    f

--- src/components/SaludoHijo.js
+++ src/components/SaludoHijo.js
@@ -2,7 +2,7 @@ function SaludoHijo(props){
     var ejecutarPadre = props.metodoPadre;
 return (<div> 
     <h1 style = {{color:"blue"}}> Saludo HIJO</h1>
-    <button onClick = {  () => ejecutarPadre(" DAVID") }> LLamar Padre </button>
+    <button onClick = {  () => ejecutarPadre(" dAVID") }> LLamar Padre </button>
 </div>)
 }
 
